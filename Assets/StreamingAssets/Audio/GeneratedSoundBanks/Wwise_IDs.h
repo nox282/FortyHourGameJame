aba9107b38e40eaf3de33fa3f76bce6b70331c4c
nox282 <nox.tux@gmail.com>
2018-03-14 18:31:10 -0400

    fixed wwise integration

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,72 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID FEET = 3200958457U;
+        static const AkUniqueID PATIENT_COUGH = 3549217275U;
+        static const AkUniqueID PATIENT_FART = 2670306474U;
+        static const AkUniqueID PATIENT_VOMIT = 918452968U;
+        static const AkUniqueID PICKUP_CORK = 2229127467U;
+        static const AkUniqueID PICKUP_GENERIC = 217759215U;
+        static const AkUniqueID PICKUP_GLUE = 1313627535U;
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
+        static const AkUniqueID PLAYER_FAILURE = 3289865841U;
+        static const AkUniqueID PLAYER_SUCCESS = 1977201850U;
+        static const AkUniqueID START_HAIRDRYER = 63629884U;
+        static const AkUniqueID STOP_HAIRDRYER = 1425259882U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace GAME_START
+        {
+            static const AkUniqueID GROUP = 733168346U;
+
+            namespace STATE
+            {
+                static const AkUniqueID GAMEPLAY = 89505537U;
+                static const AkUniqueID MAIN_TITLE = 654742305U;
+            } // namespace STATE
+        } // namespace GAME_START
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID TIMER = 3920142940U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SOUNDBNK = 1211995789U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID PLAYER_FAILURE = 3289865841U;
+        static const AkUniqueID PLAYER_SUCCESS_DIALOUGE = 3782269379U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID COMMUNICATION = 530303819U;
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
